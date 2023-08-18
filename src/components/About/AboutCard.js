cee4792d65205bd96db2ac0e290470ba937059f6
Mehul <80029739+Mehulrk7432@users.noreply.github.com>
2023-08-19 00:07:59 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,8 +10,7 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Mehul Bartwal </span>
             from <span className="purple"> Chandigarh, India.</span>
-            <br /> 😎 I am Full-Stack Developer with a passion for Science and Human Behavior.
-            <br /> Pursuing Btech in Computer Science Engineering from Lovely Professional University.
+            <br /> 😎 I am Full-Stack Developer with a passion for Science and Human Behavior and pursuing Btech in Computer Science Engineering from Lovely Professional University.
             <br /> 🫠 A punctual, goal-oriented, creative mind and graduate possessing excellent communication skills, problem-solving, and leadership skills while developing group projects.
             <br /> 👾 Highly focused, efficient, adaptable in any kind of situation, and capable of making the right decisions.
             <br /> 🔫 Shoot me an Email or add me on Linkedin :)
